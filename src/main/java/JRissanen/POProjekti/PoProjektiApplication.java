b19a733399d0f53f6624f8c3d2d46c0db9e60f67
juhkiz <bgp040@myy.haaga-helia.fi>
2021-10-28 12:55:12 +0300

    first commit

---
+++ src/main/java/JRissanen/POProjekti/PoProjektiApplication.java
@@ -0,0 +1,13 @@
+package JRissanen.POProjekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoProjektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JRissanen/POProjekti/PoProjektiApplicationTests.java
@@ -0,0 +1,13 @@
+package JRissanen.POProjekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoProjektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
